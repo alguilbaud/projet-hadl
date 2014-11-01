38d3ad1591cce1c39c3ff479594fb12cc41f6762
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@7c7e47e5-4d30-83d9-fead-19348aa6f935>
2014-11-01 11:58:41 +0000

    aggregations finies

--- trunk/HADL/src/m2/SimpleConnector.java
+++ trunk/HADL/src/m2/SimpleConnector.java
@@ -1,5 +1,33 @@
 package m2;
 
-public class SimpleConnector extends Connector {
+import java.util.ArrayList;
 
+public class SimpleConnector extends Connector {
+	private ArrayList<Role> tabRole;
+	private ArrayList<Glue> tabGlue;
+	
+	public SimpleConnector(){
+		tabRole = new ArrayList<Role>();
+		tabGlue = new ArrayList<Glue>();
+	}
+	
+	public void addRole(Role r){
+		tabRole.add(r);
+	}
+	public void removeRole(int i){
+		tabRole.remove(i);
+	}
+	public Role getRole(int i){
+		return tabRole.get(i);
+	}
+	
+	public void addGlue(Glue g){
+		tabGlue.add(g);
+	}
+	public void removeGlue(int i){
+		tabGlue.remove(i);
+	}
+	public Glue getGlue(int i){
+		return tabGlue.get(i);
+	}
 }
