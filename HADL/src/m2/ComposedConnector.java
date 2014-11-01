a081a8782636e201f7c098bb0063966a55118213
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@7c7e47e5-4d30-83d9-fead-19348aa6f935>
2014-11-01 17:16:09 +0000

    début hashmap

--- HADL/src/m2/ComposedConnector.java
+++ HADL/src/m2/ComposedConnector.java
@@ -1,36 +1,36 @@
 package m2;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ComposedConnector extends Connector {
-	private ArrayList<Connector> tabConnector;
-	private ArrayList<Role> tabRole;
+	private HashMap<String, Connector> mapConnector;
+	private HashMap<String, Role> mapRole;
 	
 	public ComposedConnector(String s, Role r1, Role r2){
 		super(s);
-		tabConnector = new ArrayList<Connector>();
-		tabRole = new ArrayList<Role>();
-		tabRole.add(r1);
-		tabRole.add(r2);
+		mapConnector = new HashMap<String, Connector>();
+		mapRole = new HashMap<String, Role>();
+		mapRole.put(r1.getName(), r1);
+		mapRole.put(r2.getName(), r2);
 	}
 	
-	public void addConnector(Connector c){
-		tabConnector.add(c);
+	public void putConnector(Connector c){
+		mapConnector.put(c.getName(), c);
 	}
-	public void removeConnector(int i){
-		tabConnector.remove(i);
+	public void removeConnector(String s){
+		mapConnector.remove(s);
 	}
-	public Connector getConnector(int i){
-		return tabConnector.get(i);
+	public Connector getConnector(String s){
+		return mapConnector.get(s);
 	}
 	
-	public void addRole(Role r){
-		tabRole.add(r);
+	public void putRole(Role r){
+		mapRole.put(r.getName(), r);
 	}
-	public void removeRole(int i){
-		tabRole.remove(i);
+	public void removeRole(String s){
+		mapRole.remove(s);
 	}
-	public Role getRole(int i){
-		return tabRole.get(i);
+	public Role getRole(String s){
+		return mapRole.get(s);
 	}
 }
