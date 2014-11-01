b9a31c8a06f3731d3d33c7b9371593264375e545
grouhan.benjamin@gmail.com <grouhan.benjamin@gmail.com@7c7e47e5-4d30-83d9-fead-19348aa6f935>
2014-11-01 18:35:10 +0000

    Classe System

--- trunk/HADL/src/m1/System.java
+++ trunk/HADL/src/m1/System.java
@@ -4,5 +4,41 @@ import m2.Configuration;
 
 public class System extends Configuration {
 	
+	public System(String name){
+		super(name);
+	}
 	
+	
+	public void addServer(Server s){
+		putElement(s);
+	}
+	public void addClient(Client c){
+		putElement(c);
+	}
+	public void addRPC(RPC r){
+		putConnector(r);
+	}
+	public void addAttachmentIn(AttachmentIn a){
+		putAttachment(a);
+	}
+	public void addAttachmentOut(AttachmentOut a){
+		putAttachment(a);
+	}
+	
+	
+	public void removeServer(String s){
+		removeElement(s);
+	}
+	public void removeClient(String c){
+		removeElement(c);
+	}
+	public void removeRPC(String r){
+		removeConnector(r);
+	}
+	public void removeAttachmentIn(String a){
+		removeAttachment(a);
+	}
+	public void removeAttachmentOut(String a){
+		removeAttachment(a);
+	}
 }
