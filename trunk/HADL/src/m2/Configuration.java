35350d1ce5b8bc5f1b40a971924a15bc68a25317
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@7c7e47e5-4d30-83d9-fead-19348aa6f935>
2014-11-01 17:24:46 +0000

    fin conversion hashmap

--- trunk/HADL/src/m2/Configuration.java
+++ trunk/HADL/src/m2/Configuration.java
@@ -3,68 +3,68 @@ package m2;
 import java.util.HashMap;
 
 public class Configuration extends Element{
-	private HashMap<String, Element> tabElement;
-	private HashMap<String, Binding> tabBinding;
-	private HashMap<String, InterfaceConf> tabInterfaceConf;
-	private HashMap<String, Attachment> tabAttachment;
-	private HashMap<String, Connector> tabConnector;
+	private HashMap<String, Element> mapElement;
+	private HashMap<String, Binding> mapBinding;
+	private HashMap<String, InterfaceConf> mapInterfaceConf;
+	private HashMap<String, Attachment> mapAttachment;
+	private HashMap<String, Connector> mapConnector;
 	
 	public Configuration(String s){
 		super(s);
-		tabElement = new HashMap<String, Element>();
-		tabBinding = new HashMap<String, Binding>();
-		tabInterfaceConf = new HashMap<String, InterfaceConf>();
-		tabAttachment = new HashMap<String, Attachment>();
-		tabConnector = new HashMap<String, Connector>();
+		mapElement = new HashMap<String, Element>();
+		mapBinding = new HashMap<String, Binding>();
+		mapInterfaceConf = new HashMap<String, InterfaceConf>();
+		mapAttachment = new HashMap<String, Attachment>();
+		mapConnector = new HashMap<String, Connector>();
 	}
 	
 	public void putElement(Element e){
-		tabElement.put(e.getName(), e);
+		mapElement.put(e.getName(), e);
 	}
 	public void removeElement(String s){
-		tabElement.remove(s);
+		mapElement.remove(s);
 	}
 	public Element getElement(String s){
-		return tabElement.get(s);
+		return mapElement.get(s);
 	}
 	
 	public void putBinding(Binding b){
-		tabBinding.put(b.getName(), b);
+		mapBinding.put(b.getName(), b);
 	}
 	public void removeBinding(String s){
-		tabBinding.remove(s);
+		mapBinding.remove(s);
 	}
 	public Binding getBinding(String s){
-		return tabBinding.get(s);
+		return mapBinding.get(s);
 	}
 	
 	public void putInterfaceConf(InterfaceConf ic){
-		tabInterfaceConf.put(ic.getName(), ic);
+		mapInterfaceConf.put(ic.getName(), ic);
 	}
 	public void removeInterfaceConf(String s){
-		tabInterfaceConf.remove(s);
+		mapInterfaceConf.remove(s);
 	}
 	public InterfaceConf getInterfaceConf(String s){
-		return tabInterfaceConf.get(s);
+		return mapInterfaceConf.get(s);
 	}
 
 	public void putAttachment(Attachment a){
-		tabAttachment.put(a.getName(), a);
+		mapAttachment.put(a.getName(), a);
 	}
 	public void removeAttachment(String s){
-		tabAttachment.remove(s);
+		mapAttachment.remove(s);
 	}
 	public Attachment getAttachment(String s){
-		return tabAttachment.get(s);
+		return mapAttachment.get(s);
 	}
 	
 	public void putConnector(Connector c){
-		tabConnector.put(c.getName(), c);
+		mapConnector.put(c.getName(), c);
 	}
 	public void removeConnector(String s){
-		tabConnector.remove(s);
+		mapConnector.remove(s);
 	}
 	public Connector getConnector(String s){
-		return tabConnector.get(s);
+		return mapConnector.get(s);
 	}
 }
